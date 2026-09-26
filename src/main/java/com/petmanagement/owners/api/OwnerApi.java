10d90ad4745e35d02f529fa2dd8ea73438b7b469
Zair Bulos <buloszair@gmail.com>
2026-09-26 11:39:46 -0300

    feat(owners): add getOwnerIdByEmail to OwnerApiService

--- src/main/java/com/petmanagement/owners/api/OwnerApi.java
+++ src/main/java/com/petmanagement/owners/api/OwnerApi.java
@@ -1,5 +1,9 @@
 package com.petmanagement.owners.api;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface OwnerApi {
     boolean existsByEmail(String email);
+    Optional<UUID> getOwnerIdByEmail(String email);
 }
--- src/main/java/com/petmanagement/owners/application/service/OwnerApiService.java
+++ src/main/java/com/petmanagement/owners/application/service/OwnerApiService.java
@@ -6,6 +6,9 @@ import com.petmanagement.owners.domain.model.valueobject.Email;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 class OwnerApiService implements OwnerApi {
 
@@ -21,6 +24,14 @@ class OwnerApiService implements OwnerApi {
         return repository
                 .findByEmail(new Email(email))
                 .isPresent();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<UUID> getOwnerIdByEmail(String email) {
+        return repository
+                .findByEmail(new Email(email))
+                .map(owner -> owner.getId().value());
     }
 
 }
